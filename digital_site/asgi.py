dd62384b7cafdaf02c3446a116c68c157b7b15c2
pediw <salehip57@gmail.com>
2025-04-28 11:50:32 -0700

    products model add

--- digital_site/asgi.py
+++ digital_site/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for diggital_products project.
+ASGI config for digital_site project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diggital_products.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digital_site.settings')
 
 application = get_asgi_application()
